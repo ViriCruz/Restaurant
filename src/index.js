5cbc1c07e7e03b2157c59a9bfed15c011e3a4869
viricruz <viridiana.cruz.olivera@gmail.com>
2020-01-24 23:45:56 -0700

    Add tab switching logic to render the correct module.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import home from './home';
 import header from './header';
+import menu from './menu';
 
 const content = document.getElementById('content');
-header(content);
-home(content);
-
+content.appendChild(header());
+content.appendChild(home());
 
 // tab switching logic
+document.getElementById('tab-home').addEventListener('click', () => {
+  const child = document.getElementById('main-content');
+  content.removeChild(child);
+  content.appendChild(home());
+});
+
+document.getElementById('tab-menu').addEventListener('click', () => {
+  const child = document.getElementById('main-content');
+  content.removeChild(child);
+  content.appendChild(menu());
+});
