62879ca3f45260b6c62eeeefac72956ad7de1627
viricruz <viridiana.cruz.olivera@gmail.com>
2020-01-28 01:44:47 -0700

    Refactor code.

--- src/header.js
+++ src/header.js
@@ -1,22 +1,13 @@
-function navItem(linkName) {
-  const li = document.createElement('li');
-  li.classList.add('nav-tab');
-  const link = document.createElement('a');
-  link.setAttribute('href', '#');
-  link.setAttribute('id', `tab-${linkName}`);
-  link.innerHTML = linkName;
-  li.appendChild(link);
-  return li;
-}
+import listItem from './listItem';
 
 const header = () => {
   const div = document.createElement('div');
   const header = document.createElement('header');
   const nav = document.createElement('nav');
   const ul = document.createElement('ul');
-  ul.appendChild(navItem('home'));
-  ul.appendChild(navItem('menu'));
-  ul.appendChild(navItem('contact'));
+  ul.appendChild(listItem('nav-tab', 'tab-home', 'home'));
+  ul.appendChild(listItem('nav-tab', 'tab-menu', 'menu'));
+  ul.appendChild(listItem('nav-tab', 'tab-contact', 'contact'));
   nav.appendChild(ul);
   header.appendChild(nav);
   div.appendChild(header);
--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,45 @@
+import listItem from './listItem';
+
 const menu = () => {
   const div = document.createElement('div');
   div.setAttribute('id', 'main-content');
-  const p = document.createElement('p');
-  p.innerHTML = 'Here goes the menu';
-  div.appendChild(p);
+  const ul = document.createElement('ul');
+  ul.setAttribute('class', 'dish-list');
+  let img = document.createElement('img');
+  img.src = '../dist/assets/images/maki-sushi.jpg';
+  img.alt = 'Maki sushi';
+  let li = listItem('dish', 'maki-sushi', 'Maki sushi');
+  let desc = li.childNodes[0];
+  let p = document.createElement('p');
+  p.innerHTML = 'jsahjhdjashdkjasd';
+  li.appendChild(img);
+  ul.appendChild(li);
+  li = listItem('dish', 'norimaki-sushi', 'Norimaki sushi');
+  desc.appendChild(p);
+  img = document.createElement('img');
+  img.src = '../dist/assets/images/norimaki.jpg';
+  img.alt = 'Norimaki sushi';
+  li.appendChild(img);
+  ul.appendChild(li);
+  li = listItem('dish', 'oshizushi-sushi', 'Oshizushi sushi');
+  img = document.createElement('img');
+  img.src = '../dist/assets/images/oshizushi.jpg';
+  img.alt = 'Oshizushi sushi';
+  li.appendChild(img);
+  ul.appendChild(li);
+  li = listItem('dish', 'uramaki-sushi', 'Uramaki sushi');
+  img = document.createElement('img');
+  img.src = '../dist/assets/images/uramaki.jpg';
+  img.alt = 'Uramaki sushi';
+  li.appendChild(img);
+  ul.appendChild(li);
+  li = listItem('dish', 'temaki-sushi', 'Temaki sushi');
+  img = document.createElement('img');
+  img.src = '../dist/assets/images/temaki.jpg';
+  img.alt = 'Temaki sushi';
+  li.appendChild(img);
+  ul.appendChild(li);
+  div.appendChild(ul);
   return div;
 };
 
