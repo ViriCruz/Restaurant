7b3a0e35766cd0a4229a8c66183e2bc55a5d70ef
viricruz <viridiana.cruz.olivera@gmail.com>
2020-02-28 10:24:55 -0700

    Add contact module

--- src/contact.js
+++ src/contact.js
@@ -1,41 +1,65 @@
 const form = () => {
-  const form = document.createElement('form')
-  form.classList.add('contact-form')
-  return form
-}
+  const element = document.createElement('form');
+  element.classList.add('contact-form');
+  return element;
+};
 
 const label = (text, name) => {
-  const label = document.createElement('label')
-  label.for = name
-  label.textContent = text
-  return label
-}
+  const element = document.createElement('label');
+  element.for = name;
+  element.textContent = text;
+  return element;
+};
 
 const input = (element, id) => {
-  const input = document.createElement(`${element}`)
-  
-  input.id = id
-  input.name = id
-  return input
-}
+  const inputForm = document.createElement(`${element}`);
+  inputForm.id = id;
+  inputForm.name = id;
+  return inputForm;
+};
+
+const contactInformation = () => {
+  const div = document.createElement('div');
+  div.classList.add('information-container');
+  const phone = document.createElement('p');
+  phone.textContent = '01179 231122';
+  const span = document.createElement('span');
+  span.textContent = 'Phone:';
+  const addressLabel = document.createElement('span');
+  addressLabel.textContent = 'Address:';
+  addressLabel.classList.add('contact-details-label');
+  span.classList.add('contact-details-label');
+  const location = document.createElement('p');
+  location.textContent = 'Carter Cres';
+  const country = document.createElement('p');
+  country.textContent = 'Gymea Bay NSW 2227, Australia';
+  div.appendChild(span);
+  div.appendChild(phone);
+  div.appendChild(addressLabel);
+  div.appendChild(location);
+  div.appendChild(country);
+  return div;
+};
 
 const contactForm = () => {
-  const formContainer = document.createElement('div')
-  formContainer.id = 'main-content'
-  const formElement = form()
-  const subjectContainer = document.createElement('div')
-  subjectContainer.appendChild(label('Subject:', 'subject'))
-  subjectContainer.appendChild(input('input', 'subject'))
-  const bodyContainer = document.createElement('div')
-  bodyContainer.appendChild(label('Message:', 'message'))
-  bodyContainer.appendChild(input('textarea', 'message'))
-  formElement.appendChild(subjectContainer)
-  formElement.appendChild(bodyContainer)
-  formElement.appendChild(input('button','submit')).textContent = "Submit"
-  formContainer.appendChild(formElement)
-  console.log(formContainer);
-  
-  return formContainer
-}
+  const formContainer = document.createElement('div');
+  formContainer.id = 'main-content';
+  const contactContainer = document.createElement('div');
+  contactContainer.classList.add('contact');
+  const formElement = form();
+  const subjectContainer = document.createElement('div');
+  subjectContainer.appendChild(label('Subject:', 'subject'));
+  subjectContainer.appendChild(input('input', 'subject'));
+  const bodyContainer = document.createElement('div');
+  bodyContainer.appendChild(label('Message:', 'message'));
+  bodyContainer.appendChild(input('textarea', 'message'));
+  formElement.appendChild(subjectContainer);
+  formElement.appendChild(bodyContainer);
+  formElement.appendChild(input('button', 'submit')).textContent = 'Submit';
+  contactContainer.appendChild(formElement);
+  contactContainer.appendChild(contactInformation());
+  formContainer.appendChild(contactContainer);
+  return formContainer;
+};
 
-export default contactForm
+export default contactForm;
